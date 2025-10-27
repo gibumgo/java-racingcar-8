f91e89e2c6c5023d0fa7ff9fbe02a49cc598e02b
gibumgo <gibumgo@gmail.com>
2025-10-27 22:31:19 +0900

    test: 자동차 이름 검증 및 예외 처리 테스트 추가

--- src/main/java/racingcar/exception/RacingGameErrorMessages.java
+++ src/main/java/racingcar/exception/RacingGameErrorMessages.java
@@ -3,7 +3,9 @@ package racingcar.exception;
 public enum RacingGameErrorMessages {
     CAR_NAME_LENGTH_ERROR("자동차 이름은 1자 이상 5자 이하만 가능합니다."),
     TRY_COUNT_ERROR("시도 횟수는 0 이상의 값이여야 합니다."),
-    POSITION_LENGTH_ERROR("위치 값은 0 이상이어야 합니다.");
+    POSITION_LENGTH_ERROR("위치 값은 0 이상이어야 합니다."),
+    NAME_DUPLICATE_ERROR("자동차 이름은 중복될 수 없습니다."),
+    CAR_NAME_EMPTY_ERROR("자동차 이름은 값이 존재해야 합니다.");
 
     private final String message;
 
--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -2,8 +2,12 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import racingcar.exception.RacingGameErrorMessages;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class CarsTest {
     @DisplayName("쉼표로 구분된 문자열을 Cars로 변환")
@@ -11,5 +15,22 @@ class CarsTest {
     void createCarsFromString() {
         Cars cars = Cars.from("pobi,crong,honux");
         assertThat(cars).isNotNull();
+    }
+
+    @DisplayName("자동차 이름이 비어있으면 예외 발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void validateEmptyNames(String names) {
+        assertThatThrownBy(() -> Cars.from(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(RacingGameErrorMessages.CAR_NAME_EMPTY_ERROR.getMessage());
+    }
+
+    @DisplayName("자동차 이름이 중복되면 예외 발생")
+    @Test
+    void validateDuplicateNames() {
+        assertThatThrownBy(() -> Cars.from("pobi,crong,pobi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(RacingGameErrorMessages.NAME_DUPLICATE_ERROR.getMessage());
     }
 }
