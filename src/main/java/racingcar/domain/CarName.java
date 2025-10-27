e4be43d281f417dfc4d6fdd04389b03a9cb85be5
gibumgo <gibumgo@gmail.com>
2025-10-27 21:35:38 +0900

    feat: 자동차 이름 유효성 검증 추가

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,8 +1,12 @@
 package racingcar.domain;
 
+import racingcar.exception.RacingGameErrorMessages;
+import racingcar.utils.StringUtils;
+
 import java.util.Objects;
 
 public class CarName {
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
 
     private CarName(String name) {
@@ -10,7 +14,14 @@ public class CarName {
     }
 
     public static CarName from(String name) {
+        validateLength(name);
         return new CarName(name);
+    }
+
+    private static void validateLength(String name) {
+        if (StringUtils.isBlank(name) || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(RacingGameErrorMessages.CAR_NAME_LENGTH_ERROR.getMessage());
+        }
     }
 
     @Override
