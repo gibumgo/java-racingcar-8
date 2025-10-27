daf3cc97b69c66e5678c319e3118e6b9e5a0146b
gibumgo <gibumgo@gmail.com>
2025-10-27 21:44:00 +0900

    test: 시도 횟수 유효성 검증 테스트 추가

--- src/test/java/racingcar/domain/TryNumberTest.java
+++ src/test/java/racingcar/domain/TryNumberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class TryNumberTest {
     @Test
@@ -12,4 +13,9 @@ class TryNumberTest {
         assertThat(TryNumber.create(1)).isEqualTo(TryNumber.create(1));
     }
 
+    @Test
+    @DisplayName("0 미만이면 예외 발생")
+    void invalid() {
+        assertThatThrownBy(() -> TryNumber.create(-1)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
