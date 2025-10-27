eaa3f4e3ea017f46e907ca51214671f9d7e28cfc
gibumgo <gibumgo@gmail.com>
2025-10-27 22:53:41 +0900

    feat: 우승자 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -10,6 +10,8 @@ public class OutputView {
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String PROGRESS_BAR = "-";
     private static final String SEPARATOR = " : ";
+    private static final String WINNER_PRINT_MESSAGE = "최종 우승자";
+    private static final String DELIMITER_COMMA = ", ";
 
     public void printResultMessage() {
         System.out.println();
@@ -21,5 +23,12 @@ public class OutputView {
             System.out.println(car.name() + SEPARATOR + StringUtils.repeat(car.position(), PROGRESS_BAR));
         }
         System.out.println();
+    }
+
+    public void printWinners(List<CarDto> winners) {
+        String winnerNames = winners.stream()
+                .map(CarDto::name)
+                .collect(Collectors.joining(DELIMITER_COMMA));
+        System.out.printf(WINNER_PRINT_MESSAGE + SEPARATOR + winnerNames + "%n");
     }
 }
