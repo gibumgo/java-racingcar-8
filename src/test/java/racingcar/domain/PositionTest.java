e37ccb64bf9b44e9a00005b0b9bb879fff07d9fe
gibumgo <gibumgo@gmail.com>
2025-10-27 21:50:59 +0900

    test : 자동차 위치 유효성 테스트 추가

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -2,8 +2,10 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.exception.RacingGameErrorMessages;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PositionTest {
     @Test
@@ -12,4 +14,11 @@ class PositionTest {
         assertThat(Position.create()).isEqualTo(Position.create());
     }
 
+    @Test
+    @DisplayName("음수 예외 발생")
+    void invalid() {
+        assertThatThrownBy(() -> Position.create(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(RacingGameErrorMessages.POSITION_LENGTH_ERROR.getMessage());
+    }
 }
