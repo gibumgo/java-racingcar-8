7bcab530d75b101d8fad2ec7a8d5e3e460221bca
gibumgo <gibumgo@gmail.com>
2025-10-27 21:35:52 +0900

    test : 자동차 이름 유효성 검증 테스트 추가

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -2,8 +2,12 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import racingcar.exception.RacingGameErrorMessages;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class CarNameTest {
     @Test
@@ -11,5 +15,22 @@ class CarNameTest {
     void create() {
         CarName carName = CarName.from("pobi");
         assertThat(carName).isEqualTo(CarName.from("pobi"));
+    }
+
+    @Test
+    @DisplayName("이름이 6자 이상이면 예외 발생")
+    void createCarNameWithTooLongName() {
+        assertThatThrownBy(() -> CarName.from("javaji"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(RacingGameErrorMessages.CAR_NAME_LENGTH_ERROR.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("빈 값이면 예외 발생")
+    @NullAndEmptySource
+    void emptyOrNullNameShouldThrowException(String name) {
+        assertThatThrownBy(() -> CarName.from(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(RacingGameErrorMessages.CAR_NAME_LENGTH_ERROR.getMessage());
     }
 }
